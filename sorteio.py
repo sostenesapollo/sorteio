d655f834c94db8a6bf6a5306b48b9c928d2d6d92
Sóstenes Apollo <sostenesapollo25@gmail.com>
2022-05-09 08:24:20 -0300

    fix: add max and min, and do not include 0

--- sorteio.py
+++ sorteio.py
@@ -1,13 +1,16 @@
 import random
 
-qnt_numbers = int(input("Quantos números você quer sortear?"))
+min_value = int(input("Qual o intervalo de números para sortear? \nDe:"))
+max_value = int(input("Até:"))
+
+qnt_chooses = int(input("Quantos números você quer sortear?\n"))
 
 _list_already_choosed = []
-_list = list(range(qnt_numbers))
+_list = list(range(min_value, max_value+1))
 
 representation = ['0️⃣ ','1️⃣ ', '2️⃣ ', '3️⃣ ', '4️⃣ ', '5️⃣ ', '6️⃣ ', '7️⃣ ', '8️⃣ ', '9️⃣ ']
 
-for i in range(qnt_numbers):
+for i in range(qnt_chooses):
     input('Sorteando numero...')
     choosed = random.choice(_list)
     number_represented = "".join([representation[int(i)] for i in str(choosed)])
